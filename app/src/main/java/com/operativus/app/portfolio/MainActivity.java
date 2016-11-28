2f94bc739626c838cff1f54f328279d0f04f29b4
Gustavo Kuhn Andriotti <gustavo.andriotti@gmail.com>
2016-11-27 16:01:20 -0800

    Portfolio design Modified .gitignore

---
+++ app/src/main/java/com/operativus/app/portfolio/MainActivity.java
@@ -0,0 +1,15 @@
+package com.operativus.app.portfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.operativus.app.portofolio.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
